5dce171a4ffdfddfe6c499927acc880b2f4c935a
dethreat1 <97066830+dethreat1@users.noreply.github.com>
2022-08-22 18:41:35 -0500

    second commit

--- app/src/main/java/android/example/arcticfox/UI/MainActivity.java
+++ app/src/main/java/android/example/arcticfox/UI/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //add line of code for git test
     }
 
     public void EnterHere(View view) {
